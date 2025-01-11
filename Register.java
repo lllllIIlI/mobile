2add370a830fc6b11e8ecc07522f196916025483
lllllIIlI <93465102+lllllIIlI@users.noreply.github.com>
2025-01-11 18:47:56 +0900

    Update Register.java

--- Register.java
+++ Register.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
